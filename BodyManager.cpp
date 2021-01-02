d854afb10c7eec168acfba87508357f17cae2d15
unai <unaidiazdegaraio@gmail.com>
2021-01-02 11:53:57 +0100

    collider
    collider

--- BodyManager.cpp
+++ BodyManager.cpp
@@ -102,7 +102,7 @@ Planet* bodyManager::CreatePlanet(Vec2 pos, int rad, float mass, float radius, V
 	planet->radius = radius;
 	planet->localGravity = localGravity;
 	planet->boodyTexture = Texture;
-	App->collisions->addCollider(rad, colliderType::planet, this, pos.x, pos.y);
+	planet->Collider=App->collisions->addCollider(rad, colliderType::planet, this, pos.x, pos.y);
 	bodyList.add(planet);
 	return planet;
 }
