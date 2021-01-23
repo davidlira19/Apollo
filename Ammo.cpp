a9b36dcb1870637e410c384edf48d872b8299585
adrianam4 <61042878+adrianam4@users.noreply.github.com>
2021-01-23 11:12:16 +0100

    Revert "Revert "improved ammo""
    This reverts commit 4c73d8a79fed01ba6315473b67d8e5a221dd3711.

--- Ammo.cpp
+++ Ammo.cpp
@@ -5,21 +5,35 @@ Ammo::Ammo(SDL_Texture* adTexture)
 {
 	ammoTexture = adTexture;
 
-	ammoAnimation.PushBack({ 16,20,107,107 });
-	ammoAnimation.PushBack({ 169,6,107,107 });
-	ammoAnimation.PushBack({ 331,4,107,107 });
-	ammoAnimation.PushBack({ 486,12,107,107 });
-	ammoAnimation.PushBack({ 611,22,107,107 });
-	ammoAnimation.PushBack({ 742,30,107,107 });
+	ammoAnimation.PushBack({ 0,0,79,66 });
+	ammoAnimation.PushBack({ 86,0,164-86,71 });
+	ammoAnimation.PushBack({ 171,0,245-171,80 });
+	ammoAnimation.PushBack({ 332,0,388-332,80 });
+	ammoAnimation.PushBack({ 402,0,461-402,79 });
 
-	ammoAnimation.PushBack({ 17,159,107,107 });
-	ammoAnimation.PushBack({ 165,150,107,107 });
-	ammoAnimation.PushBack({ 335,152,107,107 });
-	ammoAnimation.PushBack({ 488,151,107,107 });
-	ammoAnimation.PushBack({ 598,150,107,107 });
-	ammoAnimation.PushBack({ 741,161,107,107 });
+	ammoAnimation.PushBack({ 0,76,66,157-76 });
+	ammoAnimation.PushBack({ 85,82,157-85,161-82 });
+	ammoAnimation.PushBack({ 171,82,247-171,156-82 });
+	ammoAnimation.PushBack({ 254,83,334-254,150-83 });
+	ammoAnimation.PushBack({ 345,86,427-345,142-86 });
 
-	ammoAnimation.speed = 0.15f;
+	ammoAnimation.PushBack({ 5,169,86-5,228-169 });
+	ammoAnimation.PushBack({ 95,167,176-95,235-167 });
+	ammoAnimation.PushBack({ 182,164,260-182,236-164 });
+	ammoAnimation.PushBack({ 265,154,340-265,232-154 });
+	ammoAnimation.PushBack({ 354,152,421-354,232-152 });
+
+	ammoAnimation.PushBack({ 11,240,67-11,323-240 });
+	ammoAnimation.PushBack({ 94,242,153-94,323-242 });
+	ammoAnimation.PushBack({ 158,240,226-158,320-240 });
+	ammoAnimation.PushBack({ 236,240,308-236,319-240 });
+	ammoAnimation.PushBack({ 316,243,393-316,319-243 });
+	ammoAnimation.PushBack({ 405,249,486-405,316-249 });
+
+	ammoAnimation.PushBack({ 2,330,84-2,387-330 });
+	ammoAnimation.PushBack({ 102,328,183-102,388-328 });
+
+	ammoAnimation.speed = 0.2f;
 	ammoAnimation.loop = true;
 	
 	currentAnimation = &ammoAnimation;
